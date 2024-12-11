f97269543695a78d7761bad576110b2fcd12baf3
Valerian Hizkia <valhize@Valerians-MacBook-Pro.local>
2024-12-12 00:41:57 +0700

    Done problem 01 02 03 05 06 07

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    new_string = ""
+    if (num < 0) {
+        return "ERROR"
+    }
+    for (i = 0; i < num; i++) {
+        new_string = new_string + string
+    }
+    return new_string
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if (start < 0 || end < 0) {
+        return "ERROR"
+    }
+    if (!(Number.isInteger(start)) || !(Number.isInteger(end))) {
+        return "ERROR"
+    }
+    let sum = 0;
+    let first = Math.min(start, end)
+    let last = Math.max(start, end)
+    for (i = first; i < last + 1; i ++) {
+        sum = sum + i
+    }
+    return sum;
 };
 
 // Do not edit below this line
