b443509c04e8e2cbcca4a814e589e25337b84e43
Valerian Hizkia <valhize@Valerians-MacBook-Pro.local>
2024-12-25 19:54:45 +0700

    Done exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0
+	for (let i = 0; i < array.length; i++) {
+    sum = sum + array[i]
+  }
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let sum = 1
+	for (let i = 0; i < array.length; i++) {
+    sum = sum * array[i]
+  }
+  return sum
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  total = 1
+	for (let i = a; i > 1; i--) {
+    total = total * i
+  }
+  return total
 };
 
 // Do not edit below this line
